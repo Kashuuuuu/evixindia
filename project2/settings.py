029c6ea531edd57eb1bf4ca354cefd9c0ddd74e7
unknown <karunabunkar637@gmail.com>
2022-06-06 06:48:51 -0400

    initial commit

--- project2/settings.py
+++ project2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$lbka5ruqm&1dj9z0_nm0&0v%&th01mf(a=f=zn2gh4da+euql
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,13 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [BASE_DIR,"static"]
+# STATICFILES_DIRS = [BASE_DIR,"static"]
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
