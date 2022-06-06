32ba7e04f8baec1355bb7bc7e506a97e5bf0f43d
unknown <karunabunkar637@gmail.com>
2022-06-06 08:01:55 -0400

    changes commit

--- project2/urls.py
+++ project2/urls.py
@@ -19,7 +19,7 @@ from project2 import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url
+from django.conf.url import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,4 +32,5 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
+urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
