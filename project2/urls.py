319ab58e5ca8688077d599ecd7c196fdd96f4ee2
unknown <karunabunkar637@gmail.com>
2022-06-06 08:11:09 -0400

    changes commit

--- project2/urls.py
+++ project2/urls.py
@@ -18,8 +18,6 @@ from django.urls import path,include
 from project2 import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,8 +27,5 @@ urlpatterns = [
     path('', include('pro.urls')),
     path('', include('product.urls')),
     path('dashboard/', include('dashboard.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     
-]
-urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
